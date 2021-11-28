ccbd4227d150a8ef06655338c25bb600ff7ec85d
unknown <44147809+LSabin23@users.noreply.github.com>
2021-11-28 16:59:45 -0500

    added questions prompt information and function to write the README file

---
+++ index.js
@@ -0,0 +1,16 @@
+// TODO: Include packages needed for this application
+// Inquirer is required for this challenge
+
+// TODO: Create an array of questions for user input
+const questions = []
+
+// TODO: Create a function to write README file
+function writeToFile (fileName, data) {}
+// fileName will be README.md ?
+// data will be from generateMarkdown fxn in generateMarkdown.js
+
+// TODO: Create a function to initialize app
+function init () {}
+
+// Function call to initialize app
+init()
