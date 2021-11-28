ac0aac29ebed425f148461c46b17455a01eb2cd4
unknown <44147809+LSabin23@users.noreply.github.com>
2021-11-28 18:38:01 -0500

    added README markdown static and dynamic content to generateMarkdown function

---
+++ dist/README.md
@@ -0,0 +1,34 @@
+
+  # Project Title ABCD
+
+  ## Description
+  Brief description goes in this spot. Give it some additional information. Keep typing cause you're bored.
+
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Contact](#contact)
+
+  ## Installation
+  Run the installer from your terminal.
+
+  ## Usage
+  Use the product.
+
+  ## License
+  License A
+
+  ## Contributing
+  Contributions can be made.
+
+  ## Tests
+  Test it with care.
+
+  ## Contact
+  GitHub: [LSabin23](https://github.com/LSabin23/)
+  Send an email to [LSabin23's email](mailto:dev.l.email@fakemail.com) with any questions!
+  
--- index.js
+++ index.js
@@ -1,16 +1,152 @@
 // TODO: Include packages needed for this application
-// Inquirer is required for this challenge
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = []
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project\'s title?',
+    validate: titleInput => {
+      if (titleInput) {
+        return true
+      } else {
+        console.log('Please enter your project title.')
+        return false
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Briefly describe your project.',
+    validate: descInput => {
+      if (descInput) {
+        return true
+      } else {
+        console.log('Please enter a project description.')
+        return false
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'installSteps',
+    message: 'Provide installation steps for your project.',
+    validate: installInput => {
+      if (installInput) {
+        return true
+      } else {
+        console.log('You need to provide installation instructions.')
+        return false
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information for your project.',
+    validate: usageInput => {
+      if (usageInput) {
+        return true
+      } else {
+        console.log('You need to provide usage information.')
+        return false
+      }
+    }
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license.',
+    choices: [ 'License A', 'License B', 'License C', 'License D', 'License E' ]
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'How should others contribute to your project?',
+    validate: contributeInput => {
+      if (contributeInput) {
+        return true
+      } else {
+        console.log('Please identify how others can contribute.')
+        return false
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'testSteps',
+    message: 'What are the steps to test your project?',
+    validate: testInput => {
+      if (testInput) {
+        return true
+      } else {
+        console.log('Please enter one or more tests for your project.')
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'gitHubUsername',
+    message: 'Please provide your GitHub username for the README\'s contact section.',
+    validate: usernameInput => {
+      if (usernameInput) {
+        return true
+      } else {
+        console.log('You must enter your GitHub username.')
+        return false
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Please enter your email address for the README\'s contact section.',
+    validate: emailInput => {
+      if (emailInput) {
+        return true
+      } else {
+        console.log('You must provide your email address.')
+        return false
+      }
+    }
+  }
+]
 
 // TODO: Create a function to write README file
-function writeToFile (fileName, data) {}
-// fileName will be README.md ?
-// data will be from generateMarkdown fxn in generateMarkdown.js
+const writeToFile = data => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/README.md', data, err => {
+      if (err) {
+        reject(err)
+        return
+      }
+
+      resolve({
+        ok: true,
+        message: 'File created.'
+      })
+    })
+  })
+}
 
 // TODO: Create a function to initialize app
-function init () {}
+function init () {
+  // ask the user questions about their project
+  inquirer.prompt(questions)
+    // take user input and create markdown content
+    .then(projectData => {
+      return generateMarkdown(projectData)
+    })
+    // take markdown content and create md file
+    .then(writeToFile)
+    .catch(err => {
+      console.log(err)
+    })
+}
 
 // Function call to initialize app
 init()
